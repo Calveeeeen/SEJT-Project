89d57bc2e2e7c30f959360c6330ca788b0cae4d4
akrasner26 <akrasner26@gmail.com>
2021-11-11 00:28:39 -0500

    new working project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
+import Navbar from './components/navbar';
 import './App.css';
+import Dashboard from './pages/Dashboard';
+import { BrowserRouter as Router,Routes, Route } from 'react-router-dom';
+import ContactUs from './pages/ContactUs';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Dashboard />} />
+        <Route path='contact-us' element={<ContactUs />} />
+      </Routes>
+       </div>
+    </Router>
   );
 }
 
