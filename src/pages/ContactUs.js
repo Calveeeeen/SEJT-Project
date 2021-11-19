134639c08b905b795d1fde523ece33cd81c6e0f9
akrasner26 <akrasner26@gmail.com>
2021-11-18 22:45:36 -0500

    added the ContactUs JS and CSS

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,11 +1,63 @@
-import React from 'react';
+import React, { useState } from "react";
+import styles from "./ContactUs.module.css";
 
-function ContactUs(){
-    return (
-        <div>
-        <h1> hello </h1>
+const ContactUs = (props) => {
+  const handleSubmitForms = () => {
+    // Usually you would do field validation &
+    // send this to a rest endpoint to the back end here.
+    console.log(name);
+    console.log(email);
+    console.log(message);
+    setName("");
+    setEmail("");
+    setMessage("");
+  };
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  return (
+    <div className={styles.contactUsOuterContainer}>
+      <form onSubmit={handleSubmitForms}>
+      <div className={styles.nameContainer}>
+          <label className={styles.nameLabel}>
+            Name:
+          </label>
+            <input
+              className={styles.nameInput}
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
         </div>
-    )
-}
+        <div className={styles.emailContainer}>
+          <label className={styles.emailLabel}>
+            Email:
+          </label>
+            <input
+              className={styles.emailInput}
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+        </div>
+        <div className={styles.messageContainer}>
+          <label className={styles.messageLabel}>
+            Message:</label>
+            <textarea
+              className={styles.messageInput}
+              type="text"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            />
+        </div>
+        <div className={styles.submitContainer}>
+          <input className={styles.submitInput} type="submit" value="Submit" />
+        </div>
+      </form>
+    </div>
+  );
+};
 
-export default ContactUs
+export default ContactUs;
