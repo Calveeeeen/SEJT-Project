e0618ecfc9199e098ec90470842cf69be79522c4
Calveeeeen <72275469+Calveeeeen@users.noreply.github.com>
2021-12-06 20:48:31 -0500

    Dropdown styling
    played around with some dropdown styling for the font. decided to comment it out because it was causing more problems.

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -4,17 +4,25 @@ import Autocomplete from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
 
 
+
 const Dropdown = () => {
 
     return (
         <div className={styles.DropdownContainer}>
             <Autocomplete
+                // onchange
                 className={styles.ComparisonList}
                 disablePortal
                 id="Comparison List"
                 options={['New York, NY', 'Los Angeles, CA', 'Chicago, IL', 'Seattle, WA', 'San Francisco, CA', 'Austin, TX', 'Las Vegas, NV', 'C++', 'Python','Java','Javascript', 'C#','Ruby', 'Node.js', 'Microsoft office', 'React', 'React Native', 'SQL', 'Firebase', 'Figma', 'AWS']}
                 sx={{ width: 300 }}
-                renderInput={(params) => <TextField {...params} label="Comparison" />}
+                renderInput={(params) => 
+                <TextField 
+                {...params} 
+                label="Comparison"
+                // inputProps={{style: {fontSize:20}}} 
+                // InputLabelProps={{style: {fontSize: 20}}}
+                />}
             />
         </div >
     );
